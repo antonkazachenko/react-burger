1ab5ef7d9b337e2683aaf247c97f9c858202422d
creamlaflare <antonkazachenko2002@gmail.com>
2023-11-05 09:26:45 -0800

    Migrate useForm and useModal hooks to TypeScript

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,24 @@
+import { useState, ChangeEvent } from 'react';
+
+type TFormValues = {
+  [key: string]: string;
+};
+
+type TFormFunctions = {
+  values: TFormValues;
+  handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  setValues: (values: TFormValues) => void;
+};
+
+function useForm(inputValues: TFormValues = {}): TFormFunctions {
+  const [values, setValues] = useState<TFormValues>(inputValues);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  return { values, handleChange, setValues };
+}
+
+export default useForm;
