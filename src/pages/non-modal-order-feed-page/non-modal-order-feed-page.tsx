24f4b4efd061059635a33b452a31221e20927aac
creamlaflare <antonkazachenko2002@gmail.com>
2023-11-29 14:30:32 -0800

    Updated design for non-modal order feed page

--- src/pages/non-modal-order-feed-page/non-modal-order-feed-page.tsx
+++ src/pages/non-modal-order-feed-page/non-modal-order-feed-page.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { CurrencyIcon, FormattedDate } from '@ya.praktikum/react-developer-burger-ui-components';
 import { useParams } from 'react-router-dom';
 import MoonLoader from 'react-spinners/MoonLoader';
-import styles from '../../components/order-feed/order-feed.module.css';
+import styles from './non-modal-order-feed-page.module.css';
 import { TOrder } from '../../services/reducers/order-feed';
 import { useDispatch, useSelector } from '../../hooks';
 import { getOrderByID } from '../../services/actions/order-feed';
@@ -39,52 +39,70 @@ const NonModalOrderFeedPage = () => {
       </div>
     );
   }
-
+  // TODO: fix margins to adaptivity
   return (
-    <div className="ml-10 mr-10">
-      <div className="mt-10 mb-3">
-        <p className="text text_type_main-medium">{orderData.name}</p>
+    <div className={styles.cardFlex}>
+      <div className={styles.orderNumberMargin}>
+        <p className="text text_type_digits-medium">
+          #
+          {orderData.number}
+        </p>
       </div>
-      <div className="mb-15">
-        <p className={`${styles.statusColor} text text_type_main-default`}>{orderData.status}</p>
-      </div>
-      <div className="mb-6">
-        <p className="text text_type_main-medium">Состав:</p>
-      </div>
-      <div className={styles.overflow}>
-        {
-          orderData.ingredients.map((item, index) => (
-            // eslint-disable-next-line react/jsx-key
-            <div className={`${styles.imagesContainer} ${index !== 0 ? 'mt-4' : ''}`}>
-              <div className={styles.ingredientName}>
-                <div
-                  className={styles.cardImages}
-                  style={{
-                    // eslint-disable-next-line no-underscore-dangle
-                    backgroundImage: `url(${ingredients.find((ingredient) => ingredient._id === item)?.image})`,
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                  }}
-                />
-                {/* eslint-disable-next-line no-underscore-dangle */}
-                <p className="text text_type_main-default ml-4">{ingredients.find((ingredient) => ingredient._id === item)?.name}</p>
-              </div>
-              <div className={styles.ingredientName}>
-                {/* eslint-disable-next-line no-underscore-dangle */}
-                <p className="text text_type_digits-medium mr-2 ml-4">{ingredients.find((ingredient) => ingredient._id === item)?.price}</p>
-                <CurrencyIcon type="primary" />
-              </div>
-            </div>
-          ))
-        }
-      </div>
-      <div className={styles.cardFooter}>
-        <div>
-          <p className="text text_type_main-default text_color_inactive">Вчера, 13:50</p>
+      <div className="ml-10 mr-10">
+        <div className="mt-10 mb-3">
+          <p className="text text_type_main-medium">{orderData.name}</p>
         </div>
-        <div className={`${styles.ingredientName} mb-5`}>
-          <p className="text text_type_digits-medium mr-2 ml-4">{calculateTotalPrice(orderData)}</p>
-          <CurrencyIcon type="primary" />
+        <div className="mb-15">
+          <p className={`${styles.statusColor} text text_type_main-default`}>{orderData.status}</p>
+        </div>
+        <div className="mb-6">
+          <p className="text text_type_main-medium">Состав:</p>
+        </div>
+        <div className={styles.overflow}>
+          {
+            orderData.ingredients.map((item, index) => (
+              // eslint-disable-next-line react/jsx-key
+              <div className={`${styles.imagesContainer} ${index !== 0 ? 'mt-4' : ''}`}>
+                <div className={styles.ingredientName}>
+                  <div
+                    className={styles.cardImages}
+                    style={{
+                      // eslint-disable-next-line no-underscore-dangle
+                      backgroundImage: `url(${ingredients.find((ingredient) => ingredient._id === item)?.image})`,
+                      backgroundPosition: 'center',
+                      backgroundSize: 'cover',
+                    }}
+                  />
+                  <p
+                    className="text text_type_main-default ml-4"
+                  >
+                    {/* eslint-disable-next-line no-underscore-dangle */}
+                    {ingredients.find((ingredient) => ingredient._id === item)?.name}
+                  </p>
+                </div>
+                <div className={styles.ingredientName}>
+                  <p
+                    className="text text_type_digits-medium mr-2 ml-4"
+                  >
+                    {/* eslint-disable-next-line no-underscore-dangle */}
+                    {ingredients.find((ingredient) => ingredient._id === item)?.price}
+                  </p>
+                  <CurrencyIcon type="primary" />
+                </div>
+              </div>
+            ))
+          }
+        </div>
+        <div className={styles.cardFooter}>
+          <div>
+            <p className="text text_type_main-default text_color_inactive">
+              <FormattedDate date={new Date(orderData.createdAt)} />
+            </p>
+          </div>
+          <div className={`${styles.ingredientName} mb-5`}>
+            <p className="text text_type_digits-medium mr-2 ml-4">{calculateTotalPrice(orderData)}</p>
+            <CurrencyIcon type="primary" />
+          </div>
         </div>
       </div>
     </div>
